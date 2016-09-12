b24cdf04d001617a34828dee8dd581ff4ed1f771
gajal <gajal.agarwala4@gmail.com>
2016-09-12 10:27:33 +0000

    complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,7 +69,7 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation, :admin)
+                                   :password_confirmation)
     end
 
 
